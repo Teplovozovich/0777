12d5b31ec7d2a57a0952d6254c6fde7580a681c9
Teplovozovich <artur.dmitriev.2016@inbox.ru>
2023-12-20 20:55:53 +0500

    f

---
+++ WpfApp1/List.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для List.xaml
+    /// </summary>
+    public partial class List : Window
+    {
+        public List()
+        {
+            InitializeComponent();
+            var itemSource = TestEntities.GetContext().Goods.ToList();
+            
+            ListExport.ItemsSource = itemSource;
+            //itemSource.Insert(0, new Type)
+        }
+
+        private void ListExport_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void GoodsCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void SearchBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateGoods();
+        }
+
+        private void UpdateGoods()
+        {
+            var currentGoods = TestEntities.GetContext().Goods.ToList();
+            if (GoodsCombo.SelectedIndex > 0)
+            {
+                //currentGoods = currentGoods.Where(x => x.sale.Contains(GoodsCombo.SelectedItem as Type)).ToList();
+
+            }
+            currentGoods = currentGoods.Where(x => x.name_goods.ToLower().Contains(SearchBox.Text.ToLower())).ToList();
+            ListExport.ItemsSource = currentGoods;
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (TestEntities db = new TestEntities())
+            {
+                List<Users> user = (from us in db.Users where us.login == LoginInput.Text && us.password == PasswordInput.Password select us).ToList();
+
+                if (user.Count != 0)
+                {
+                    if (user[0].role == "Администратор")
+                    {
+                        Admin admin = new Admin();
+                        this.Close();
+                        admin.Show();
+                    }
+                } else
+                {
+                    MessageBox.Show("aboba");
+                }
+            }
+            
+        }
+
+        private void GuestButton_Click(object sender, RoutedEventArgs e)
+        {
+            List list = new List();
+            list.Show();
+        }
+    }
+}
